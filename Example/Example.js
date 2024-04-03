dfd37e78b3571050e91d8a7866425e9c0c0d90c1
Bertrand D <b.duclot@gmail.com>
2024-04-03 17:29:45 +0200

    init

---
+++ Example/Example.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>click me</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return <p>You have read {props.count} pieces of advice </p>;
+}
