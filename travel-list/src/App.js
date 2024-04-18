7f3bea374e2502efc181ebc6e04198e821bb5593
Bertrand D <b.duclot@gmail.com>
2024-04-18 11:50:28 +0200

    update

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,9 +1,16 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
       <Logo />
       <Form />
       <PackingList />
+      <Stats />
     </div>
   );
 }
@@ -18,12 +25,33 @@ function Form() {
     </div>
   );
 }
+
 function PackingList() {
-  return <div className="List">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>🧳You have X items on your List, and you already packed X (X%)</em>
     </footer>
   );
