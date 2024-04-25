7b788200124f117f741e4356cac0785fe25d4000
Bertrand D <b.duclot@gmail.com>
2024-04-25 21:37:55 +0200

    update

--- steps/src/App.js
+++ steps/src/App.js
@@ -30,7 +30,7 @@ function Steps() {
       </button>
       {isOpen && (
         <div className="steps">
-          <div class="numbers">
+          <div className="numbers">
             <div className={step >= 1 ? "active" : ""}>1</div>
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
