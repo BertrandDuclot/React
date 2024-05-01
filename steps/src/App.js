6807ef832f8bdb10fec22f62575da823d97ba817
Bertrand D <b.duclot@gmail.com>
2024-05-01 17:46:27 +0200

    update

--- steps/src/App.js
+++ steps/src/App.js
@@ -41,15 +41,19 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlePrevious}>
-              Previous
-            </button>
-            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleNext}>
-              Next
-            </button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious} text="Previous" />
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext} text="Next" />
           </div>
         </div>
       )}
     </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, text }) {
+  return (
+    <button style={{ backgroundColor: bgColor, color: textColor }} onClick={onClick}>
+      {text}
+    </button>
   );
 }
