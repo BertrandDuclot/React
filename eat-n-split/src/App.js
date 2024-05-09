32f74202c619dfc8c62a0769ab79967f13dfc29f
Bertrand D <b.duclot@gmail.com>
2024-05-09 21:33:52 +0200

    update

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -87,13 +87,30 @@ function Friend({ friend }) {
 }
 
 function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const id = crypto.randomUUID();
+
+    const newFriend = {
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    console.log(newFriend);
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>🧑‍🤝‍🧑Friend name</label>
-      <input type="text" />
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
 
       <label>🏜️Image URL</label>
-      <input type="text" />
+      <input type="text" value={image} onChange={(e) => setImage(e.target.value)} />
 
       <Button>Add</Button>
     </form>
